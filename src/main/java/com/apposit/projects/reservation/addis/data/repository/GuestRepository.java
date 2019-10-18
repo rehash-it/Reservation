9c75a9e670b306f8e34c069c053075dc4f0b4a9c
Rehana Abdulber <rehusmail@gmail.com>
2019-10-18 15:41:39 +0300

    Guest Repository Added

---
+++ src/main/java/com/apposit/projects/reservation/addis/data/repository/GuestRepository.java
@@ -0,0 +1,8 @@
+package com.apposit.projects.reservation.addis.data.repository;
+
+import com.apposit.projects.reservation.addis.data.entity.Guest;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface GuestRepository extends PagingAndSortingRepository<Guest,Long> {
+
+}
