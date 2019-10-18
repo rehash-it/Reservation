04ab8570aa18bd963d9061730a7c1aa2dced6692
Rehana Abdulber <rehusmail@gmail.com>
2019-10-18 15:12:29 +0300

    Room Repository Added

--- src/main/java/com/apposit/projects/reservation/addis/data/repository/RoomRepository.java
+++ src/main/java/com/apposit/projects/reservation/addis/data/repository/RoomRepository.java
@@ -1,4 +1,11 @@
 package com.apposit.projects.reservation.addis.data.repository;
 
-public interface RoomRepository {
+import com.apposit.projects.reservation.addis.data.entity.Room;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends CrudRepository<Room,Long> {
+    Room findByNumber(String number);
+
 }
